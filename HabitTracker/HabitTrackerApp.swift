ab7fa0d1be881f66ac8a600b4a1390b592267866
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-26 18:59:58 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Cesar Lopez on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
